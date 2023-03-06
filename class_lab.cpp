7d721e976c1b503e7429de6c4459aa171a6ae5bd
Kenny <125699311+kenyaver@users.noreply.github.com>
2023-03-06 12:57:43 +0300

    1-5lab

--- class_lab.cpp
+++ class_lab.cpp
@@ -4,14 +4,23 @@
 using namespace std;
 
 class person {
- protected:
+protected:
+  string L_name;
+
+private:
   string name = "\0";
   int age = 0;
 
- public:
-  person(string a_name = "\0", int a_age = 0) {
-    name = a_name;
-    age = a_age;
+public:
+  person(string Lname = "\0", string name = "\0", int age = 0) {
+    L_name = Lname;
+    this->name = name;
+    this->age = age;
+  }
+
+  string set_Lname(string Lname = "\0") {
+    L_name = Lname;
+    return L_name;
   }
 
   string set_name(string _name) {
@@ -24,75 +33,104 @@ class person {
     return age;
   }
 
-  void get_name() { cout << "name is " << name << "\n"; }
+  void get_name() { cout << name << "\t"; }
 
-  void get_age() { cout << name << "`s age is " << age << "\n"; }
+  void get_age() { cout << age << "\t"; }
 
-  ~person() { cout << name << " was killed(\n"; }
+  void get_Lname() { cout << L_name << '\t'; }
+
+  // ~person() { cout << name << " was killed(\n"; }
 };
 
-class high_person : public person {
- private:
-  double high = 0;
+class children : public person {
+private:
+  string name;
+  int age;
+  // string L_name = person::L_name;
+public:
 
- public:
-  high_person(string a_name = "qw", int a_age = 0, double a_high = 0) {
-    name = a_name;
-    age = a_age;
-    high = a_high;
+  children(string F_name = "\0", int age = 0) {
+    // children::L_name = person::L_name; 
+    this->name = F_name;
+    this->age = age;
   }
 
-  string set_name(string _name) {
-    name = _name;
+  string set_Fname(string Fname) {
+    name = Fname;
     return name;
   }
 
-  int set_age(int _age) {
-    age = _age;
-    return age;
+  // string set_Lname(){
+  //   children::L_name = person::L_name;
+  //   return children::L_name;
+  // }
+
+  int set_age(int age) {
+    children::age = age;
+    return children::age;
   }
 
-  double set_high(double _high) {
-    high = _high;
-    return high;
-  }
+  void get_Fname() { cout << name << '\t'; }
 
-  void get_name() { cout << "name is " << name << '\n'; }
+  // void get_Lname() { cout << L_name << '\t'; }
 
-  void get_age() { cout << name << "`s age is " << age << '\n'; }
-
-  void get_high() { cout << name << "`s high is " << high << '\n'; }
-
-  // ~high_person() { cout << name << " was killed)\n"; }
+  void get_age() { cout << age << '\t'; }
 };
 
 int main() {
-  string nam;
-  int ag;
-  cout << "cin name:\n";
-  cin >> nam;
-  cout << "cin age:\n";
-  cin >> ag;
-  cout << '\n';
-
-  person a;
-  a.set_name(nam);
-  a.set_age(ag);
+  person a("ar", "te", 18);
+  a.set_name("artem");
+  a.set_Lname("garipov");
+  a.set_age(19);
   a.get_name();
+  a.get_Lname();
   a.get_age();
-  a.~person();
+
   cout << '\n';
 
-  person b("Bob", 34);
-  b.get_name();
+  children b("ni", 80);
+  b.set_Fname("nikita");
+  b.set_age(8);
+  b.set_Lname("garipov");
+  b.get_Fname();
+  b.get_Lname();
   b.get_age();
-  cout << '\n';
-
-  high_person c;
-  c.get_name();
-  c.get_age();
-  c.get_high();
   cout << '\n';
 
   return 0;
 }
+
+// class high_person : public person {
+//  private:
+//   double high = 0;
+
+//  public:
+//   high_person(string a_name = "qw", int a_age = 0, double a_high = 0) {
+//     name = a_name;
+//     age = a_age;
+//     high = a_high;
+//   }
+
+//   string set_name(string _name) {
+//     name = _name;
+//     return name;
+//   }
+
+//   int set_age(int _age) {
+//     age = _age;
+//     return age;
+//   }
+
+//   double set_high(double _high) {
+//     high = _high;
+//     return high;
+//   }
+
+//   void get_name() { cout << "name is " << name << '\n'; }
+
+//   void get_age() { cout << name << "`s age is " << age << '\n'; }
+
+//   void get_high() { cout << name << "`s high is " << high << '\n'; }
+
+//   // ~high_person() { cout << name << " was killed)\n"; }
+// };
